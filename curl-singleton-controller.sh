33c15fd14d326bb557112f89db4a19838f703b23
Michael McNichol <mmcnichol@gogoair.com>
2017-12-21 23:28:55 -0600

    Add sleep between curl commands to prevent race
    condition

--- curl-singleton-controller.sh
+++ curl-singleton-controller.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 curl localhost:8080/work?do=ThingOne &
+sleep .25 # Provide time for initial call to set ComponentIsSingleton.stringValue and Sleep
 curl localhost:8080/morework?do=ThingTwo
